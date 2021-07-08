e02a84455831845200b81ad6463b04bef9c443d6
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-07-08 13:28:23 -0600

    🎨 Add necessary files for creating/editing blogs

---
+++ scripts/manage/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import Helpers  from '../helpers.js';
+import HtmlFactory from '../patterns/factory.js';
+import { loadNavbar, loadFooter } from '../sharedScripts.js';
+import '../sharedHtmlElements.js';
+
+const helpers = new Helpers();
+
+//[FUNCTIONS]
+const loadHtml = async () => {
+  const navbar = await loadNavbar();
+  const footer = await loadFooter();
+
+  navbarContainer.innerHTML = navbar;
+  footerContainer.innerHTML = footer;
+};
+
+//[TRIGGERS]
+loadHtml();
